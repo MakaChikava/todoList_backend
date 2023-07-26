9c3731f2bdd20e7f23b297fa2a29dfe378754fb1
makachikava <chikavamakanaka@gmail.com>
2023-07-25 21:03:12 -0700

    completed functioning CRUD routes

--- server.js
+++ server.js
@@ -19,9 +19,20 @@ app.get('/', async (req, res)=>{
 })
 
 // Create
-app.post('/todo', async (req, res)=>)
+app.post('/todo', async (req, res)=>{
+    const newTodo = await Todo.create(req.body);
+    res.json(newTodo);
+})
 // Edit
+app.put('/todo/:id', async (req, res)=>{
+    const updateTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, {new:true});
+    res.json(updateTodo);
+})
 // Delete
+app.delete('/todo/:id', async (req, res)=>{
+    const deleteTodo = await Todo.findByIdAndRemove(req.params.id);
+    res.json(deleteTodo);
+})
 // 
 
 
